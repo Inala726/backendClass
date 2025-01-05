01f722d4289e17c6339be68bd4b268610d214396
Inala <inalajacob740@gmail.com>
2025-01-05 01:25:41 +0100

    removed uneccessary imports

--- src/dtos/createCourse.dto.ts
+++ src/dtos/createCourse.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsNumber, Length, Max, Min } from "class-validator";
+import { IsNotEmpty, IsNumber, Length} from "class-validator";
 
 export class CreateCourseDTO{
     @IsNotEmpty()
