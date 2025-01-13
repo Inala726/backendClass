4d74d14644866ebfab5de070b3e70fd8de12c13f
Inala <inalajacob740@gmail.com>
2025-01-13 12:38:46 +0100

    type shit

--- src/routes/userRoute.routes.ts
+++ src/routes/userRoute.routes.ts
@@ -1,12 +1,14 @@
 import express from "express"
 import { UserController } from "../controllers/userController.control";
+import { AuthController } from "../controllers/auth.controller";
 const userController = new UserController();
+const authController = new AuthController()
 const userRouter = express.Router();
 
 userRouter.post("/", userController.createUser);
 userRouter.get("/", userController.getAllUsers);
 userRouter.get("/:id", userController.getUserById);
-
+userRouter.post("/", authController.login)
 userRouter.put("/:id", userController.updateUser)
 userRouter.delete("/:id", userController.deleteUser)
 
--- src/utils/password.utils.ts
+++ src/utils/password.utils.ts
@@ -1,7 +1,14 @@
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
 
-const SALT_ROUNDS = 10
+const SALT_ROUNDS = 10;
 
-export const hashPassword = async (password:string) =>{
-    return await bcrypt.hash(password, SALT_ROUNDS)
-}
+export const hashPassword = async (password: string) => {
+  return await bcrypt.hash(password, SALT_ROUNDS);
+};
+
+export const comparePassword = async (
+  password: string,
+  hashPassword: string
+) => {
+  return await bcrypt.compare(password, hashPassword);
+};
